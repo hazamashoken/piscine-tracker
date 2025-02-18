dc8c7b46f94ae366d4751345d6b64fc1b518ce80
hazamashoken <earth78203@gmail.com>
2025-02-18 16:07:05 +0800

    add location migrations
    Change-Id: I0c30f83d5cff75a3529f424e410ec39d248ec7c8
    Signed-off-by: hazamashoken <earth78203@gmail.com>

---
+++ pocketbase/pb_migrations/1739860407_created_location.js
@@ -0,0 +1,88 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": "",
+    "deleteRule": "",
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "date942028621",
+        "max": "",
+        "min": "",
+        "name": "begin_at",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "date"
+      },
+      {
+        "hidden": false,
+        "id": "date936579196",
+        "max": "",
+        "min": "",
+        "name": "end_at",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "date"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text3475444733",
+        "max": 0,
+        "min": 0,
+        "name": "host",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_1724955765",
+        "hidden": false,
+        "id": "relation2375276105",
+        "maxSelect": 1,
+        "minSelect": 0,
+        "name": "user",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "relation"
+      }
+    ],
+    "id": "pbc_517001682",
+    "indexes": [
+      "CREATE INDEX `idx_p2G9pTGola` ON `location` (`user`)"
+    ],
+    "listRule": "",
+    "name": "location",
+    "system": false,
+    "type": "base",
+    "updateRule": "",
+    "viewRule": ""
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_517001682");
+
+  return app.delete(collection);
+})
