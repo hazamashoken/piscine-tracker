a31095aacc230391912f43a93eee502eb2697c2b
hazamashoken <earth78203@gmail.com>
2025-02-28 21:01:57 +0800

    filter out non pisciners
    Change-Id: I8b3221a10e6489f8b0c7325b4fce93fd0403dff9
    Signed-off-by: hazamashoken <earth78203@gmail.com>

--- frontend/src/routes/dashboard/+page.server.ts
+++ frontend/src/routes/dashboard/+page.server.ts
@@ -2,7 +2,8 @@ import { pb } from '$lib/pocketbase';
 export async function load() {
 	let logins = []
 	const data = await pb.collection("pisciner").getFullList({
-		sort: "login"
+		sort: "login",
+		filter: "is_pisciner=true"
 	});
 	logins = data.map((pisciner) => pisciner.login);
 	return {
